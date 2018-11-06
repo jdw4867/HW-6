f9c1f188b47a0f1796dcd135ddc11f788adc8c1e
Jonathan Walsh <jonathan.walsh@utexas.edu>
2018-11-06 17:21:03 -0600

    A few bugfixes
    still working on driver. Uploading this for version control before i try something fairly major reorganizing the driver

--- go_fish.cpp
+++ go_fish.cpp
@@ -18,76 +18,71 @@ int main() {
 
     Player p1 ("Rainey");
     Player p2 ("Jonathan");
-    int twoPlayerTotalHandSize = 7;
-    for(int i = 0; i < twoPlayerTotalHandSize; i++) {           //deals 7 cards to each player (2-player game)
+    int twoPlayerHandSize = 7;
+    for(int i = 0; i < twoPlayerHandSize; i++) {           //deals 7 cards to each player (2-player game)
         p1.addCard(deck.dealCard());
         p2.addCard(deck.dealCard());
     }
+    Card c1;
+    Card c2;
+    while(p1.checkHandForBook(c1, c2)) {
+        cout << "From the cards he drew, " << p1.getName() << " books a pair of " << c1.rankString(c1.getRank()) << "'s." << endl;
+        p1.bookCards(c1, c2);
+    }
+    cout << endl;
+    while(p2.checkHandForBook(c1, c2)) {
+        cout << "From the cards he drew, " << p2.getName() << " books a pair of " << c1.rankString(c1.getRank()) << "'s." << endl;
+        p2.bookCards(c1, c2);
+    }
 
     while(deck.size() != 0 && p1.getHandSize() != 0 && p2.getHandSize()){
-        Card c1;
-        Card c2;
-        while(p1.checkHandForBook(c1, c2)) p1.bookCards(c1, c2);
-        while(p2.checkHandForBook(c1, c2)) p2.bookCards(c1, c2);
-
+    
+        cout << p1.getName() << "'s hand: " << p1.showHand() << endl;
         c1 = p1.chooseCardFromHand();
-        cout << p1.getName() << "asks - Do you have a " << c1.getRank() << "?" << endl;
-        
+        cout << p1.getName() << " asks - Do you have a " << c1.rankString(c1.getRank()) << "?" << endl;
+        if(p2.cardInHand(c1)) {
+            cout << p2.getName() << " says - Yes. I have a " << c1.rankString(c1.getRank()) << "." << endl;
+            p1.addCard(c1);
+            p2.removeCardFromHand(c1);
+            p1.bookCards(c1, c2);
+            cout << p1.getName() << " books his pair of " << c1.rankString(c1.getRank()) << "'s." << endl << endl;
+        } else{
+            cout << "No, Go Fish." << endl;
+            p1.addCard(deck.dealCard());
+            cout << p1.getName() << " draws a " << c1.toString() << " and adds it to his hand." << endl << endl;
+            if(p1.checkHandForBook(c1, c2)) {
+                cout << "From the card he drew, " << p1.getName() << " books a pair of "
+                    << c1.rankString(c1.getRank()) << "'s." << endl << endl;
+                p1.bookCards(c1, c2);
+        }
+
+        c1 = p2.chooseCardFromHand();
+        cout << p2.getName() << " asks - Do you have a " << c1.rankString(c1.getRank()) << "?" << endl;
+        if(p1.cardInHand(c1)) {
+            cout << p1.getName() << " says - Yes. I have a " << c1.rankString(c1.getRank()) << "." << endl;
+            p2.addCard(c1);
+            p1.removeCardFromHand(c1);
+            p2.bookCards(c1, c2);
+            cout << p2.getName() << " books his pair of " << c1.rankString(c1.getRank()) << "'s." << endl << endl;
+        } else{
+            cout << "No, Go Fish." << endl;
+            p2.addCard(deck.dealCard());
+            cout << p2.getName() << " draws " << c1.toString() << " and adds it to his hand." << endl << endl;
+            if(p1.checkHandForBook(c1, c2)) {
+                cout << "From the card he drew, " << p1.getName() << " books a pair of "
+                    << c1.rankString(c1.getRank()) << "'s." << endl << endl;
+                p1.bookCards(c1, c2);
+            }
+        }
+    }
+
+    cout << "The deck is empty. Who is the winner??" << endl << endl;
+    cout << p1.getName() << " has " << (p1.getBookSize())/2 << " books. They are " << p1.showBooks() << endl;
+    cout << p2.getName() << " has " << (p2.getBookSize())/2 << " books. They are " << p2.showBooks() << endl;
+    if(p1.getBookSize() > p2.getBookSize()) {
+        cout << p1.getName() << " has more books. Player 1 is the winner!" << endl << endl;
+    }else{
+        cout << p2.getName() << " has more books. Player 2 is the winner!" << endl << endl;
     }
 }
-
-
-
-
-
-
-
-
-
-/*
-// FILE: card_demo.cpp
-// This is a small demonstration program showing how the Card and Deck classes are used.
-#include <iostream>    // Provides cout and cin
-#include <cstdlib>     // Provides EXIT_SUCCESS
-#include "card.h"
-#include "player.h"
-#include "deck.h"
-
-using namespace std;
-
-
-// PROTOTYPES for functions used by this demonstration program:
-void dealHand(Deck &d, Player &p, int numCards);
-
-
-
-
-int main( )
-{
-    int numCards = 5;
-
-    Player p1("Joe");
-    Player p2("Jane");
-
-    Deck d;  //create a deck of cards
-    d.shuffle();
-
-    dealHand(d, p1, numCards);
-    dealHand(d, p2, numCards);
-
-    cout << p1.getName() <<" has : " << p1.showHand() << endl;
-    cout << p2.getName() <<" has : " << p2.showHand() << endl;
-
-    return EXIT_SUCCESS;
-}
-
-
-
-void dealHand(Deck &d, Player &p, int numCards)
-{
-    for (int i=0; i < numCards; i++)
-        p.addCard(d.dealCard());
-}
-
-*/
 
--- player.cpp
+++ player.cpp
@@ -19,14 +19,12 @@ void Player::bookCards(Card c1, Card c2){
 }
 
 bool Player::checkHandForBook(Card &c1, Card &c2){
-    vector<Card>::const_iterator iter1;
-    vector<Card>::const_iterator iter2;
-
-    for(iter1 = myHand.begin(); iter1 != myHand.end()-1; iter1++){
-        for(iter2 = iter1+1; iter2 != myHand.end(); iter2++){
-            if(*iter1 == *iter2){
-                c1 = *iter1;
-                c2 = *iter2;
+    int size = getHandSize();
+    for(int i = 0; i < (size-1); i++){
+        for(int x = i + 1; x < size; x++){
+            if(myHand[i] == myHand[x]){
+                c1 = myHand[i];
+                c2 = myHand[x];
                 return true;
             }
         }
@@ -46,38 +44,40 @@ Card Player::chooseCardFromHand() const{
 }
 
 bool Player::cardInHand(Card c) const{
-    vector<Card>::const_iterator iter;
-    for(iter = myHand.begin(); iter != myHand.end(); iter++){
-        if (c == *iter) return true;
+    for(int i = 0; i < getHandSize(); i++){
+        if (c == myHand[i]) return true;
     }
     return false;
 }
 
 Card Player::removeCardFromHand(Card c) {
-    vector<Card>::const_iterator iter;
-    int index = 0;
-    for (iter = myHand.begin(); iter != myHand.end(); iter++) {
-        if (myHand[index] != c) index++;
+    for (int i = 0; i < getHandSize(); i++) {
+        if (myHand[i] == c) {
+            Card temp = myHand[i];
+            myHand.erase(myHand.begin() + i);
+            return temp;
+        }
     }
-    Card temp = myHand[index];
-    myHand.erase(myHand.begin() + index);
-    return temp;
 }
 
 string Player::showHand() const{
     string hand = "";
-    for(int i = 0; i < getHandSize(); i++) {
+    int numCards = getHandSize();
+    for(int i = 0; i < numCards; i++) {
         hand += myHand[i].toString();
         hand += " ";
     }
+    return hand;
 }
 
-string Player::showBooks() const{
-    string hand = "";
-    for(int i = 0; i < getBookSize(); i++) {
-        hand += myBook[i].toString();
-        hand += " ";
+string Player::showBooks() const {
+    string book = "";
+    int numCards = getBookSize();
+    for (int i = 0; i < numCards; i++) {
+        book += myBook[i].toString();
+        book += " ";
     }
+    return book;
 }
 
 int Player::getHandSize() const{
